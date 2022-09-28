ed429277d231851071e0325026bcc06a0d9b203b
ralismark <13449732+ralismark@users.noreply.github.com>
2022-09-28 13:34:32 +1000

    Fix flake-utils input

--- flake.nix
+++ flake.nix
@@ -3,11 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
